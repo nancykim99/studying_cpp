258aa37424a7d43d880e13342ef5e85f33fea26b
Nancy <mandubong1206@gmail.com>
2025-11-11 20:38:05 +0900

    Title: 윤년인가, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251111/윤년인가/is-leap-year.cpp
+++ 251111/윤년인가/is-leap-year.cpp
@@ -4,12 +4,10 @@ using namespace std;
 int main() {
     int Y;
     cin >> Y;
-    if ((Y % 4 == 0) && (Y % 100 == 0)) {
-        if (Y % 400 != 0) {
-            cout << "false";
-        } else {
-            cout << "true";
-        }
+    if ((Y % 4 == 0 && Y % 100 != 0) || (Y % 400 == 0)) {
+    cout << "true";
+    } else {
+    cout << "false";
     } 
     return 0;
 }
