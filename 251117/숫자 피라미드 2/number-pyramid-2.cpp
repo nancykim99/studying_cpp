a624386f56b8f56ab5ca611bf775f78064c926fa
Nancy <mandubong1206@gmail.com>
2025-11-17 23:09:09 +0900

    Title: 숫자 피라미드 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251117/숫자 피라미드 2/number-pyramid-2.cpp
+++ 251117/숫자 피라미드 2/number-pyramid-2.cpp
@@ -4,15 +4,16 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
-    int num_total = 0;
-    for (int i = 1; i <= 7; i++) {
-        num_total += i;
-    }
-    for (int i = 1; i <= num_total; i++) {
-        for (int j = 1; j <= N; j++) {
-            cout << j << " ";
+
+    int cur = 1;  // 출력할 현재 숫자
+
+    for (int i = 1; i <= N; i++) {        // i는 행 번호 (1~N)
+        for (int j = 0; j < i; j++) {     // 각 행에서 i개 출력
+            cout << cur << " ";
+            cur++;
         }
-        cout << endl;
+        cout << "\n";
     }
+
     return 0;
 }
