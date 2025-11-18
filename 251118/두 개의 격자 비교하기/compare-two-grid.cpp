bf9d3ce887a50a37ab0d10f6ebbc7195cf5370af
Nancy <mandubong1206@gmail.com>
2025-11-18 16:21:43 +0900

    Title: 두 개의 격자 비교하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251118/두 개의 격자 비교하기/compare-two-grid.cpp
+++ 251118/두 개의 격자 비교하기/compare-two-grid.cpp
@@ -6,19 +6,19 @@ int main() {
     cin >> n >> m;
     int matrix1[n][m];
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < m; j++) {
             cin >> matrix1[i][j];
         }
     }
     int matrix2[n][m];
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < m; j++) {
             cin >> matrix2[i][j];
         }
     }
     int ansmatrix[n][m];
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < m; j++) {
             if (matrix1[i][j] == matrix2[i][j]) {
                 ansmatrix[i][j] = 0;
             } else {
@@ -27,7 +27,7 @@ int main() {
         }
     }
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < m; j++) {
             cout << ansmatrix[i][j] << " ";
         }
         cout << endl;
