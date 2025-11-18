20ecc466cb24bab141f0d665f62c8485b2fb97dc
Nancy <mandubong1206@gmail.com>
2025-11-18 16:37:39 +0900

    Title: 특정 원소들의 합, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251118/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int matrix[4][4];
+    int total = 0;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j <= i; j++) {
+            total += matrix[i][j];
+        }
+    }
+    cout << total;
+    return 0;
+}
