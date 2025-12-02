de417b7457f24c68ca591e2450f714db9ca4ff59
Nancy <mandubong1206@gmail.com>
2025-12-02 15:39:23 +0900

    Title: 굉장한 수, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 251202/굉장한 수/amazing-number.cpp
+++ 251202/굉장한 수/amazing-number.cpp
@@ -7,6 +7,8 @@ int main() {
     cin >> n;
     if ((n % 2 = 1 && n % 3 == 0) || (n % 2 = 0 && n % 5 == 0)) {
         cout << "true";
+    } else {
+        cout << "false";
     }
     return 0;
 }
