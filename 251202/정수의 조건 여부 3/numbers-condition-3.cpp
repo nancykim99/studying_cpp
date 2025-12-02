69623bf4d0e624dd845094e03f3c47b4f0f95cd7
Nancy <mandubong1206@gmail.com>
2025-12-02 15:30:33 +0900

    Title: 정수의 조건 여부 3, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251202/정수의 조건 여부 3/numbers-condition-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    if (a % 13 == 0 || a % 19 == 0) {
+        cout << "True";
+    } else {
+        coujt << "False";
+    }
+    return 0;
+}
