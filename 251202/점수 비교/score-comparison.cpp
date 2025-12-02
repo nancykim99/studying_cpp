8cac1104a2f6cb8d7d0571faeb1c8a3197c7aabf
Nancy <mandubong1206@gmail.com>
2025-12-02 15:15:36 +0900

    Title: 점수 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251202/점수 비교/score-comparison.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int am, ae, bm, be;
+    cin >> am >> ae >> bm >> be;
+    if (am > bm && ae > be) {
+        cout << 1;
+    } else {
+        cout << 0;
+    }
+    return 0;
+}
