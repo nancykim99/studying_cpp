cc5e8f23ef169e3d09be573db1f44c144807a7b7
Nancy <mandubong1206@gmail.com>
2025-12-02 15:40:56 +0900

    Title: 굉장한 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251202/굉장한 수/amazing-number.cpp
+++ 251202/굉장한 수/amazing-number.cpp
@@ -5,7 +5,7 @@ int main() {
     // Please write your code here.
     int n;
     cin >> n;
-    if ((n % 2 = 1 && n % 3 == 0) || (n % 2 = 0 && n % 5 == 0)) {
+    if ((n % 2 == 1 && n % 3 == 0) || (n % 2 == 0 && n % 5 == 0)) {
         cout << "true";
     } else {
         cout << "false";
