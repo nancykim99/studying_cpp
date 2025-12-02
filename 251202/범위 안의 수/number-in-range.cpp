5c4bb29d28ccef121bbba9d3b80ebff1b9d12c1b
Nancy <mandubong1206@gmail.com>
2025-12-02 15:17:23 +0900

    Title: 범위 안의 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251202/범위 안의 수/number-in-range.cpp
+++ 251202/범위 안의 수/number-in-range.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int a;
     cin >> a;
-    if (10 <= a $$ a <= 20) {
+    if (10 <= a && a <= 20) {
         cout << "yes";
     } else {
         cout << "no";
