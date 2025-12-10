b40a966db78101fd9304d2119004a97ef0f57282
Nancy <mandubong1206@gmail.com>
2025-12-10 17:59:36 +0900

    Title: A부터 B까지의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251210/A부터 B까지의 합/sum-from-a-to-b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int num_sum = 0;
+    for (int i = a; i <= b; i++) {
+        num_sum += i;
+    }
+    cout << num_sum;
+    return 0;
+}
