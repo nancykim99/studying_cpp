3c09ada59f2d38cb4a9fab854a9fb2d3bb3d836e
Nancy <mandubong1206@gmail.com>
2025-12-17 22:28:39 +0900

    Title: 특정 수 맞히기, Time: 1090ms, Memory: 68MB, Status: Time Limit Exceed - Codetree

---
+++ 251217/특정 수 맞히기/catching-specific-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    while (true) {
+        cin >> n;
+        if (n > 25) {
+            cout << "Higher" << "\n";
+        } else if (n < 25) {
+            cout << "Lower" << "\n";
+        } else {
+            cout << "Good" << "\n";
+        }
+    }
+    return 0;
+}
